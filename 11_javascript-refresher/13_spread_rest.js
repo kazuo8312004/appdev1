e0042b0ce0ca4d980fab956b38673410a5bb4cda
kazuo8312004 <delpilarkirk@gmail.com>
2025-09-05 23:01:07 +0800

    13_spread_rest.js done

---
+++ 11_javascript-refresher/13_spread_rest.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4, 5];
+
+const user = { name: "Percival", age: 21 };
+const newUser = { ...user, course: "BSIS" };
+
+function sumAll(...args) {
+  return args.reduce((acc, val) => acc + val, 0);
+}
