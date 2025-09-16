ed73c719988fb847544419fdc069af9d275f64cb
kazuo8312004 <delpilarkirk@gmail.com>
2025-09-16 22:53:49 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -7,11 +7,18 @@ function MyButton() {
   );
 }
 
+const user = { 
+  name: 'Percy',              
+  age: 18,
+  course: 'BSIS-3A'
+  };
+
 function MyApp() {
   return (
-    <h1 className='message'>Welcome to my app</h1>
+    <h1 className='message'>Welcome to my {user.name} app</h1>
   );
 }
+
 
 
 function App() {
@@ -19,6 +26,7 @@ function App() {
   return (
     <div>
       <MyApp />
+      <MyButton />
     </div>
   )
 }
