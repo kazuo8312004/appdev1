a4dbcae7ab5fd9fe04d1c2aeb3776939f60e23e7
kazuo8312004 <delpilarkirk@gmail.com>
2025-10-19 01:31:07 +0800

    Add tricky parts activity

--- 11_javascript-refresher/09_tricky_parts.js
+++ 11_javascript-refresher/09_tricky_parts.js
@@ -1,28 +1,28 @@
 console.log("5" == 5);   
-console.log("5" === 5); 
+console.log("5" === 5);  
 
 let x;
 let y = null;
-console.log(typeof x, typeof y); 
+console.log(x, typeof x);
+console.log(y, typeof y); 
 
 const obj = {
-  regularFunc: function() {
+  regular: function() {
     console.log(this);
   },
-  arrowFunc: () => {
+  arrow: () => {
     console.log(this);
   }
 };
 
-obj.regularFunc();
-obj.arrowFunc();   
-
-let arr1 = [1, 2];
+obj.regular(); 
+obj.arrow();   
+let arr1 = [1, 2, 3];
 let arr2 = arr1;
-arr2.push(3);
+arr2.push(4);
 console.log(arr1); 
 
 let arr3 = [...arr1];
-arr3.push(4);
+arr3.push(5);
 console.log(arr1); 
 console.log(arr3); 
