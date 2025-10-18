f4eff2660f0196fd322f47df2d15aa286b6822d9
kazuo8312004 <delpilarkirk@gmail.com>
2025-10-19 01:33:45 +0800

    Add destructuring activity

--- 11_javascript-refresher/12_destructuring.js
+++ 11_javascript-refresher/12_destructuring.js
@@ -1,13 +1,17 @@
 const person = {
   name: "Percival",
-  age: 25
+  age: 21
 };
 
-const hobbies = ["gaming", "coding"];
+const hobbies = ["reading", "coding"];
 
 const { name, age } = person;
-const [hobby1, hobby2] = hobbies;
+const [firstHobby, secondHobby] = hobbies;
 
 function printName({ name }) {
   console.log(name);
 }
+
+console.log(name, age);
+console.log(firstHobby, secondHobby);
+printName(person);
