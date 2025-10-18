e884956eae840b5f1632df57ba1a32e7c3c9e639
kazuo8312004 <delpilarkirk@gmail.com>
2025-10-19 01:33:20 +0800

    Add arrow functions activity

--- 11_javascript-refresher/11_arrow_functions.js
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -1,3 +1,7 @@
-const greet = name => `Hello, ${name}`;
-const square = n => n * n;
+const greet = (name) => "Hello, " + name;
+const square = (n) => n * n;
 const sayHi = () => console.log("Hi!");
+
+console.log(greet("Percival"));
+console.log(square(5));
+sayHi();
