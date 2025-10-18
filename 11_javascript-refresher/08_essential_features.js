1735a592f1d4f7a3b47f82f9f7092d1467927bdd
kazuo8312004 <delpilarkirk@gmail.com>
2025-10-19 01:30:04 +0800

    Add essential features activity

--- 11_javascript-refresher/08_essential_features.js
+++ 11_javascript-refresher/08_essential_features.js
@@ -1,8 +1,9 @@
-let hobbies = ["gaming", "coding", "reading"];
+let hobbies = ["Coding", "Gaming", "Reading"];
 hobbies.map(hobby => console.log(hobby));
 
-const student = { name: "Percival", age: 21 };
-const { name, age } = student;
+let student = { name: "Percival", age: 21 };
+let { name, age } = student;
+console.log(name, age);
 
 let arr = [1, 2, 3];
 let newArr = [...arr, 4, 5];
