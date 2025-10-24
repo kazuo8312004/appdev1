8d5a06f68b6dd938479ea2eb5a54bca26b5b7e1f
kazuo8312004 <delpilarkirk@gmail.com>
2025-10-24 16:56:36 +0800

    Done with how to configure components with props

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,13 +1,10 @@
-const student = {
-  name: "Kirk Percival B. Del Pilar",
-  course: "BSIS 3-A"
-};
+import Avatar from "./component/Avatar";
 
 export default function App() {
   return (
-    <div style={{ backgroundColor: "black", color: "pink", padding: "10px" }}>
-      <h1>{student.name}'s Activity</h1>
-      <p>Course: {student.course}</p>
+    <div>
+      <Avatar name="Lei Ann" />
+      <Avatar name="Kirk Percival" />
     </div>
   );
 }
---
+++ react-describing-ui/src/component/Avatar.jsx
@@ -0,0 +1,6 @@
+export default function Avatar({ name }) {
+    return (
+      <p>Hello {name}</p>
+    );
+  }
+  
