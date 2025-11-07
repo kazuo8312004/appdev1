5b8dbaa2002750bc3d30d00fc29f27655b1b4011
kazuo8312004 <delpilarkirk@gmail.com>
2025-11-07 17:18:46 +0800

    Done with react-your-first-component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Gallery from './components/Gallery';
+
+function App() {
+  
+  return (
+    <>
+      <h1>Gallery</h1>
+      <Gallery />
+    </>
+  )
+}
+
+export default App
