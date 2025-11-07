21167fa3293f2e0ec649c9f1d0283ee7522aeade
kazuo8312004 <delpilarkirk@gmail.com>
2025-11-07 17:37:42 +0800

    Done with splitting Profile into its own file

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,10 +1,11 @@
-import Gallery, { Profile } from './components/Gallery.jsx';
+import Gallery from './components/Gallery.jsx';
+import Profile from './components/Profile.jsx';
 
 export default function App() {
   return (
-    <div>
+    <>
       <Profile />
       <Gallery />
-    </div>
+    </>
   );
 }
