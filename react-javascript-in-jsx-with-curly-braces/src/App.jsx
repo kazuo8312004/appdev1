50fe138287daad8c608d147b1c8f0463bbb83824
kazuo8312004 <delpilarkirk@gmail.com>
2025-11-22 14:55:03 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -12,6 +12,15 @@ function App() {
       alt={description}
       />
       <h1>{name}'s to do List</h1>
+
+      <ul style={{
+        backgroundColor: 'black',
+        color: 'pink',
+      }}>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
     </>
   )
 }
