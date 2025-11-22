adc4eba7246c22cf86ceecec7d8cac768b903512
kazuo8312004 <delpilarkirk@gmail.com>
2025-11-22 14:50:52 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
   const description = 'Gregorio Y. Zara';
+  const name = 'Gregorio Y. Zara';  
   return (
     <>
       <img
@@ -10,6 +11,7 @@ function App() {
       src={avatar}
       alt={description}
       />
+      <h1>{name}'s to do List</h1>
     </>
   )
 }
